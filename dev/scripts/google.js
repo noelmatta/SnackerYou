64ffd0e78ecc576d0ca860cce52e068a651f8fc7
Noel Matta <noelmatta82@gmail.com>
2018-03-06 17:52:31 -0500

    functionality done

--- dev/scripts/google.js
+++ dev/scripts/google.js
@@ -20,6 +20,7 @@ export class MapContainer extends React.Component {
         this.markerClick = this.markerClick.bind(this);
         this.onMapClicked = this.onMapClicked.bind(this);
         this.clickThis = this.clickThis.bind(this);
+        this.deleteRestaurant = this.deleteRestaurant.bind(this);
     }
     componentWillReceiveProps(props) {
         // console.log(this.props.userHistory)
@@ -59,7 +60,12 @@ export class MapContainer extends React.Component {
             })
         }
     }
-
+    deleteRestaurant(key) {
+        // e.preventDefault();
+console.log(key)
+        const dbRef = firebase.database().ref(`restaurants/${key}`)
+        dbRef.remove();
+    }
     render(props) {
         const style = {
             width:'70%',
@@ -67,17 +73,22 @@ export class MapContainer extends React.Component {
         }
         return (<div className="rightColumn">
             <div className="infoPane">
+                    <button onClick={this.clickThis} className="save">Save Restaurant</button>
 
                 {this.state.savedRestaurants.map((restaurant) => {
-console.log(restaurant.address);
-                    console.log(restaurant.key);
-                    console.log(restaurant.restaurant);
+                    console.log(restaurant)
+                    return(
+<span key={restaurant.key}>
+                        <h5>{restaurant.restaurant}</h5>
+                        <p>{restaurant.address}</p>
+                        <button value={restaurant.key} onClick={() => this.deleteRestaurant(restaurant.key)}><i class="fas fa-times"></i></button>
+</span>
+                        
+                    )
+                    
                 })}
 
-              <h5>{this.state.title}</h5>
-              <p>{this.state.address}</p>
-              {/* <span>{this.state.rating}</span> */}
-              <button onClick={this.clickThis}>CLICK CLICK</button>
+
             </div>
             <Map google={this.props.google} zoom={13} onClick={this.onMapClicked} center={this.props.coords} style={style}>
               {Object.values(this.props.locations).map(
